c64a93d376e54227747b4e584162b01247c11445
zimkada <zimkada@gmail.com>
2024-01-19 20:38:33 +0100

    Création du programme 1 (identité)

---
+++ Program1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 19 20:33:59 2024
+
+@author: ok
+"""
+
+def ton_nom (name):
+    return f'Ton nom est {name}'
+
+def ton_age(age):
+    return f'Tu as {age} ans'
